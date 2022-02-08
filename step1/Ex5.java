a2346ceba01d9671ee068fe55e7e4731b2e6f3a9
ivanchenko92 <richiilya92@gmail.com>
2022-02-08 22:28:48 +0300

    продолжаем

--- step1/Ex5.java
+++ step1/Ex5.java
@@ -2,6 +2,15 @@ package JWD.step1;
 
 public class Ex5 {
     public static void main(String[] args) {
-        
+        double arr[] = new double[3];
+        arr [0] = 5;
+        arr [1] = 17.3;
+        arr [2] = -3.0;
+
+        for (double x: arr) {
+            if (x < 0) x = Math.pow(x, 4);
+            else x *=x;
+            System.out.println(x);
+        }
     }
 }
